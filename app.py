63ee08e6747d4fe7aa421b624e95eb2498b34e5b
thecarbun <subhopriyobadyakar1412@gmail.com>
2024-11-12 20:01:06 +0530

    Adds toggle button for bts processing

--- app.py
+++ app.py
@@ -19,6 +19,8 @@ def initialize_chat_history():
       'content': "Hi! I'm AskNotes.ai. Ask me anything about the uploaded PDF!"
     }
   ]
+  if sst.show_bts:
+    st.sidebar.caption(":grey[Chat History Initialized.]")
 
 def show_chat(messages: list):
   """
@@ -33,6 +35,8 @@ def show_chat(messages: list):
       is_user=msg['role'] == 'user', 
       key=str(i)
     )
+  if sst.show_bts:
+    st.sidebar.caption(":grey[Displaying Chat.]")
 
 def add_to_chat(role, content):
   """
@@ -48,11 +52,15 @@ def add_to_chat(role, content):
       "content": content
     }
   )
+  if sst.show_bts:
+    st.sidebar.caption(":grey[Message Added to Chat History..]")
   
   message(
     message=content, 
     is_user=(role == 'user')
   )
+  if sst.show_bts:
+    st.sidebar.caption(":grey[Displaying Message..]")
 
 def get_vectorstore():
   """
@@ -61,11 +69,16 @@ def get_vectorstore():
   Returns:
       Vectorstore object stored in session state.
   """
+  if sst.show_bts:
+    st.sidebar.caption(":grey[Creating Vectorstore..]")
+
   loader_list, temp_paths = get_loader(sst.pdf_files)
   try:
     with st.spinner("Creating Vectorstore..."):
       if not loader_list:
         st.error("No valid PDF files could be processed. Try uploading another PDF.")
+        if sst.show_bts:
+          st.sidebar.caption(":red[Error while processing PDFs..]")
         return
       
       embeddings = OpenAIEmbeddings()
@@ -74,9 +87,13 @@ def get_vectorstore():
         embedding=embeddings
       ).from_loaders(loader_list)
       st.toast("Vectorstore created successfully!")
+      if sst.show_bts:
+        st.sidebar.caption(":grey[Created Vectorstore Successfully..]")
       st.rerun()
   except Exception as e:
     st.error(f"Error creating vectorstore: `{e}`. Try another PDF.")
+    if sst.show_bts:
+      st.sidebar.caption(":red[Error! Unable to create vectorstore..]")
     st.stop()
   finally:
     delete_temp_files(temp_paths)
@@ -92,6 +109,8 @@ def get_loader(pdf_files: list):
       list: PDF loaders for processing.
       list: Temporary file paths for cleanup.
   """
+  if sst.show_bts:
+    st.sidebar.caption(":grey[Processing PDFs..]")
   try:
     with st.spinner("Loading PDFs..."):
       pdf_loader_list = []
@@ -103,6 +122,8 @@ def get_loader(pdf_files: list):
             pdf_loader_list.append(PyPDFLoader(f.name))
             temp_paths.append(f.name)
         except Exception as e:
+          if sst.show_bts:
+            st.sidebar.caption(":red[Error loading PDF..]")
           st.error(f"Error loading PDF {pdf.name}: {e}")
       return pdf_loader_list, temp_paths
   except Exception as e:
@@ -118,11 +139,15 @@ def delete_temp_files(temp_paths: list):
   Args:
       temp_paths (list): Paths of temporary files to delete.
   """
+  if sst.show_bts:
+        st.sidebar.caption(":grey[Deleting Temporary files..]")
   for path in temp_paths:
     try:
       os.remove(path)
     except Exception as e:
       st.warning(f"Failed to delete temp file {path}: {e}")
+  if sst.show_bts:
+        st.sidebar.caption(":grey[Temporary Files Deleted.]")
 
 
 def load_css() -> str:
@@ -171,6 +196,12 @@ def main():
     if "vectorstore" in sst:
       if st.button("Remake Vectorstore", use_container_width=True):
         sst.pop("vectorstore", None)
+
+    if "show_bts" not in sst:
+      sst.show_bts = False
+    if st.toggle(label="Display backend activity", help="Enable detailed logging of backend processes for transparency and debugging."):
+      sst.show_bts = True
+      st.caption(":grey[Displaying background activity..]")
   # ---- ---- ---- ---- ---- ----
 
   if pdf_files:
