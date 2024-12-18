d2d955dab481f23b0371b4390df9475266781374
TheCarBun <subhopriyobadyakar1412@gmail.com>
2024-12-18 23:58:13 +0530

    Updates homepage with button and image

--- pages/home.py
+++ pages/home.py
@@ -7,16 +7,7 @@ st.markdown(f'<style>{load_css()}</style>', unsafe_allow_html=True)
 
 current_file = Path(__file__)
 css_file = current_file.parent.parent / 'static' / 'home.css'
-
-# CSS for Home Page
-if css_file.exists():
-    st.markdown(f"""
-    <style>
-          {load_css(css_file)}
-    </style>
-""", unsafe_allow_html=True)
-else:
-    print("home.css not found")
+html_file = current_file.parent.parent / 'static' / 'home.html'
 
 # ---- Navbar ----
 with st.container():
@@ -39,4 +30,27 @@ with st.container():
     )
 st.markdown("---")
 
-st.caption("Work In Progress..")
+# HTML for Home Page
+st.markdown("""
+# ✨Asknotes.ai
+### AI teacher that helps you understand your notes
+            """)
+
+# CSS for Home Page
+if css_file.exists():
+    st.markdown(f"""
+    <style>
+          {load_css(css_file)}
+    </style>
+""", unsafe_allow_html=True)
+else:
+    print("home.css not found")
+
+st.link_button(
+    label="Try for free :material/arrow_right_alt:",
+    url="https://asknotes.streamlit.app/",
+    type="primary"
+    )
+
+st.markdown("---")
+st.image("https://imgur.com/Z4FhMyg.png")
